13eba180511819ee5d1683c56852b5cca20536b1
ALmAZ <38611294+almaziphone@users.noreply.github.com>
2025-07-26 17:30:03 +0500

    Add simple audio support with TypeScript

---
+++ src/js/audio.js
@@ -0,0 +1,24 @@
+const AudioContextClass = window.AudioContext || window.webkitAudioContext;
+const audioCtx = new AudioContextClass();
+function beep(freq, duration) {
+  const oscillator = audioCtx.createOscillator();
+  const gain = audioCtx.createGain();
+  oscillator.frequency.value = freq;
+  oscillator.type = "sine";
+  oscillator.connect(gain);
+  gain.connect(audioCtx.destination);
+  oscillator.start();
+  gain.gain.exponentialRampToValueAtTime(
+    0.0001,
+    audioCtx.currentTime + duration,
+  );
+  oscillator.stop(audioCtx.currentTime + duration);
+}
+export function playSpinSound() {
+  beep(523.25, 0.1);
+}
+export function playWinSound() {
+  beep(1046.5, 0.1);
+  setTimeout(() => beep(1046.5, 0.1), 150);
+  setTimeout(() => beep(1046.5, 0.1), 300);
+}
